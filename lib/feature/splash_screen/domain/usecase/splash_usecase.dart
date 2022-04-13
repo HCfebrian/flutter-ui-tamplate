e487be75525b6953f04921bd5cd4de1eb94155db
heryan <febriansyah.online@gmail.com>
2022-04-13 07:42:59 +0700

    percepat splash screen dari 5 detik ke 1

--- lib/feature/splash_screen/domain/usecase/splash_usecase.dart
+++ lib/feature/splash_screen/domain/usecase/splash_usecase.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 class SplashUsecase {
   Future<bool> initApp() async {
-    await Future.delayed(const Duration(seconds: 5));
+    await Future.delayed(const Duration(seconds: 1));
     log('finish init');
     return true;
   }
